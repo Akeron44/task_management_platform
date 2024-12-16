a0e33981bdb0a9e074d6e8caf5bd3cdceab48adb
Akeron44 <oniakeron@gmail.com>
2024-12-16 11:20:36 +0100

    Second test

--- api/src/modules/auth/auth.module.ts
+++ api/src/modules/auth/auth.module.ts
@@ -1,16 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { UserModule } from '../user/user.module';
-import { PrismaModule } from '../prisma/prisma.module';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
-import { AuthHelper } from './helpers/auth.helper';
-import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './strategies/jwt.strategy';
-import { JwtAuthGuard } from 'src/common/guards/jwt.guard';
-import { ErrorDal } from 'src/common/dal/error.dal';
-import { UserDal } from '../user/dal/user.dal';
+import { Module } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { UserModule } from "../user/user.module";
+import { PrismaModule } from "../prisma/prisma.module";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { AuthHelper } from "./helpers/auth.helper";
+import { PassportModule } from "@nestjs/passport";
+import { JwtStrategy } from "./strategies/jwt.strategy";
+import { JwtAuthGuard } from "../../common/guards/jwt.guard";
+import { ErrorDal } from "../../common/dal/error.dal";
+import { UserDal } from "../user/dal/user.dal";
 
 @Module({
   imports: [
@@ -20,8 +20,8 @@ import { UserDal } from '../user/dal/user.dal';
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('TOKEN_SECRET_KEY'),
-        signOptions: { expiresIn: '2h' },
+        secret: configService.get<string>("TOKEN_SECRET_KEY"),
+        signOptions: { expiresIn: "2h" },
       }),
     }),
   ],
