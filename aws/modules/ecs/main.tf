aea2123155131b2f5e87c6d5a2f74025bc61479d
Akeron44 <oniakeron@gmail.com>
2024-12-17 16:40:40 +0100

    Main yml

--- aws/modules/ecs/main.tf
+++ aws/modules/ecs/main.tf
@@ -1,270 +1,16 @@
-# resource "aws_ecs_cluster" "ecs_cluster" {
-#   name = "akeron-ecs-cluster"
-# }
-
-# resource "aws_launch_template" "ecs_lt" {
-#   name_prefix   = "ecs-template"
-#   image_id      = "ami-062c116e449466e7f"
-#   instance_type = "t3.micro"
-
-#   iam_instance_profile {
-#     name = "ecsInstanceRole"
-#   }
-
-#   block_device_mappings {
-#     device_name = "/dev/xvda"
-#     ebs {
-#       volume_size = 30
-#       volume_type = "gp2"
-#     }
-#   }
-
-#   tag_specifications {
-#     resource_type = "instance"
-#     tags = {
-#       Name = "akeron-ecs-instance"
-#     }
-#   }
-
-#   user_data = filebase64("./modules/ecs/ecs.sh")
-# }
-
-# resource "aws_autoscaling_group" "ecs_asg" {
-#   min_size            = 1
-#   max_size            = 3
-#   vpc_zone_identifier = ["subnet-048ed615c746deb83", "subnet-0c446972c4be90650", "subnet-0a886674953f327bc"]
-
-#   launch_template {
-#     id      = aws_launch_template.ecs_lt.id
-#     version = "$Latest"
-#   }
-
-# }
-
-# resource "aws_lb" "ecs_alb" {
-#   name               = "akeron-alb"
-#   load_balancer_type = "application"
-#   subnets            = ["subnet-048ed615c746deb83", "subnet-0c446972c4be90650", "subnet-0a886674953f327bc"]
-
-#   tags = {
-#     Name = "akeron-alb"
-#   }
-# }
-
-# resource "aws_lb_listener" "ecs_alb_listener" {
-#   load_balancer_arn = aws_lb.ecs_alb.arn
-#   port              = 80
-#   protocol          = "HTTP"
-
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.ecs_tg.arn
-#   }
-# }
-
-# resource "aws_lb_target_group" "ecs_tg" {
-#   vpc_id      = "vpc-072bcbc3be2e0ec63"
-#   name        = "akeron-ecs-target-group"
-#   port        = 80
-#   protocol    = "HTTP"
-#   target_type = "ip"
-
-#   health_check {
-#     path = "/"
-#   }
-# }
-
-# resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
-#   name = "akeronsCapacity"
-
-#   auto_scaling_group_provider {
-#     auto_scaling_group_arn = aws_autoscaling_group.ecs_asg.arn
-
-#     managed_scaling {
-#       maximum_scaling_step_size = 1000
-#       minimum_scaling_step_size = 1
-#       status                    = "ENABLED"
-#       target_capacity           = 1
-#     }
-#   }
-# }
-
-# resource "aws_ecs_cluster_capacity_providers" "example" {
-#   cluster_name       = aws_ecs_cluster.ecs_cluster.name
-#   capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
-
-
-#   default_capacity_provider_strategy {
-#     base              = 1
-#     weight            = 100
-#     capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
-#   }
-# }
-
-# resource "aws_ecs_task_definition" "ecs_task_definition" {
-#   family = "akeron-ecs-task"
-
-#   container_definitions = jsonencode([{
-#     name  = "akeron-ecs-task"
-#     image = "863872515231.dkr.ecr.eu-central-1.amazonaws.com/akeronecr:1.0.0"
-#     cpu   = 0
-#     portMappings = [{
-#       name          = "api-5000-tcp"
-#       containerPort = 4000
-#       hostPort      = 4000
-#       protocol      = "tcp"
-#       appProtocol   = "http"
-#     }],
-#     essential = true
-#     environment = [
-#       {
-#         name  = "PORT"
-#         value = "4000"
-#       },
-#       {
-#         name  = "POSTGRES_USER"
-#         value = var.username
-#       },
-#       {
-#         name  = "POSTGRES_PASSWORD"
-#         value = var.password
-#       },
-#       {
-#         name  = "POSTGRES_DB"
-#         value = var.db_name
-#       },
-#       {
-#         name  = "POSTGRES_HOST"
-#         value = var.db_host
-#       },
-#       {
-#         name  = "POSTGRES_PORT"
-#         value = var.port
-#       },
-#       {
-#         name  = "TOKEN_SECRET_KEY"
-#         value = "task_management_db_secret_key"
-#       },
-#       {
-#         name  = "DATABASE_URL"
-#         value = "postgresql://akeron:Pcvinpostgres@postgresakeron.cbjrqddvgoeg.eu-central-1.rds.amazonaws.com:5432/akerondatabase"
-#       }
-#     ]
-#   }])
-#   execution_role_arn       = "arn:aws:iam::863872515231:role/ecsTaskExecutionRole"
-#   requires_compatibilities = ["EC2"]
-#   network_mode             = "awsvpc"
-#   cpu                      = "1024"
-#   memory                   = "2048"
-# }
-
-# resource "aws_ecs_service" "ecs_service" {
-#   name            = "akeron-ecs-service"
-#   cluster         = aws_ecs_cluster.ecs_cluster.id
-#   task_definition = aws_ecs_task_definition.ecs_task_definition.arn
-#   desired_count   = 1
-
-#   network_configuration {
-#     subnets         = ["subnet-048ed615c746deb83", "subnet-0c446972c4be90650", "subnet-0a886674953f327bc"]
-#     security_groups = ["sg-0943ad0b8875a7e25"]
-#   }
-
-#   force_new_deployment = true
-#   placement_constraints {
-#     type = "distinctInstance"
-#   }
-
-#   capacity_provider_strategy {
-#     capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
-#     weight            = 100
-#   }
-
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.ecs_tg.arn
-#     container_name   = "akeron-ecs-task"
-#     container_port   = 4000
-#   }
-
-#   depends_on = [aws_autoscaling_group.ecs_asg]
-# }
-
-resource "aws_iam_policy" "ecsPolicy" {
-  name   = "ecsAkeronRole"
-  policy = <<EOF
- {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2:DescribeTags",
-                "ecs:CreateCluster",
-                "ecs:DeregisterContainerInstance",
-                "ecs:DiscoverPollEndpoint",
-                "ecs:Poll",
-                "ecs:RegisterContainerInstance",
-                "ecs:StartTelemetrySession",
-                "ecs:UpdateContainerInstancesState",
-                "ecs:Submit*",
-                "ecr:GetAuthorizationToken",
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:BatchGetImage",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "ecs:TagResource",
-            "Resource": "*",
-            "Condition": {
-                "StringEquals": {
-                    "ecs:CreateAction": [
-                        "CreateCluster",
-                        "RegisterContainerInstance"
-                    ]
-                }
-            }
-        }
-    ]
-}
-  EOF
-}
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-  }
-}
-resource "aws_iam_role" "role" {
-  name               = "akeron-test-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-resource "aws_iam_role_policy_attachment" "ec2-read-only-policy-attachment" {
-  role       = aws_iam_role.role.name
-  policy_arn = aws_iam_policy.ecsPolicy.arn
-}
-resource "aws_iam_instance_profile" "test_profile" {
-  name = "test_role_2"
-  path = "/"
-  role = aws_iam_role.role.name
-}
-resource "aws_ecs_cluster" "ecs" {
+resource "aws_ecs_cluster" "ecs_cluster" {
   name = "akeron-ecs-cluster"
 }
-resource "aws_launch_template" "name1" {
+
+resource "aws_launch_template" "ecs_lt" {
   name_prefix   = "ecs-template"
   image_id      = "ami-062c116e449466e7f"
   instance_type = "t3.micro"
+
   iam_instance_profile {
     name = "ecsInstanceRole"
   }
+
   block_device_mappings {
     device_name = "/dev/xvda"
     ebs {
@@ -272,54 +18,68 @@ resource "aws_launch_template" "name1" {
       volume_type = "gp2"
     }
   }
+
   tag_specifications {
     resource_type = "instance"
     tags = {
-      Name = "ecs-instance"
+      Name = "akeron-ecs-instance"
     }
   }
+
   user_data = filebase64("./modules/ecs/ecs.sh")
 }
-resource "aws_autoscaling_group" "asg" {
+
+resource "aws_autoscaling_group" "ecs_asg" {
   min_size            = 1
   max_size            = 3
   vpc_zone_identifier = ["subnet-048ed615c746deb83", "subnet-0c446972c4be90650", "subnet-0a886674953f327bc"]
+
   launch_template {
-    id      = aws_launch_template.name1.id
+    id      = aws_launch_template.ecs_lt.id
     version = "$Latest"
   }
+
 }
-resource "aws_lb" "elb" {
-  name               = "akeron-lb"
+
+resource "aws_lb" "ecs_alb" {
+  name               = "akeron-alb"
   load_balancer_type = "application"
-  subnets            = ["subnet-048ed615c746deb83", "subnet-0c446972c4be90650"]
+  subnets            = ["subnet-048ed615c746deb83", "subnet-0c446972c4be90650", "subnet-0a886674953f327bc"]
+
   tags = {
-    Name = "akeron-lb"
+    Name = "akeron-alb"
   }
 }
-resource "aws_lb_listener" "name" {
-  load_balancer_arn = aws_lb.elb.arn
+
+resource "aws_lb_listener" "ecs_alb_listener" {
+  load_balancer_arn = aws_lb.ecs_alb.arn
   port              = 80
   protocol          = "HTTP"
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.ecs_tg.arn
   }
 }
+
 resource "aws_lb_target_group" "ecs_tg" {
   vpc_id      = "vpc-072bcbc3be2e0ec63"
   name        = "akeron-ecs-target-group"
   port        = 80
   protocol    = "HTTP"
   target_type = "ip"
+
   health_check {
     path = "/"
   }
 }
+
 resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
-  name = "akeronCapacity"
+  name = "akeronsCapacity"
+
   auto_scaling_group_provider {
-    auto_scaling_group_arn = aws_autoscaling_group.asg.arn
+    auto_scaling_group_arn = aws_autoscaling_group.ecs_asg.arn
+
     managed_scaling {
       maximum_scaling_step_size = 1000
       minimum_scaling_step_size = 1
@@ -328,31 +88,34 @@ resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
     }
   }
 }
+
 resource "aws_ecs_cluster_capacity_providers" "example" {
-  cluster_name       = aws_ecs_cluster.ecs.name
+  cluster_name       = aws_ecs_cluster.ecs_cluster.name
   capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
+
+
   default_capacity_provider_strategy {
     base              = 1
     weight            = 100
     capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
   }
 }
-resource "aws_ecs_task_definition" "service" {
-  family = "akeron-task"
+
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+  family = "akeron-ecs-task"
+
   container_definitions = jsonencode([{
-    "name" : "akeron-task",
-    "image" : "863872515231.dkr.ecr.eu-central-1.amazonaws.com/akeronecr:1.0.0",
-    "cpu" : 0,
-    "portMappings" : [
-      {
-        "name" : "api-5000-tcp",
-        "containerPort" : 4000,
-        "hostPort" : 4000,
-        "protocol" : "tcp",
-        "appProtocol" : "http"
-      }
-    ],
-    "essential" : true,
+    name  = "akeron-ecs-task"
+    image = "863872515231.dkr.ecr.eu-central-1.amazonaws.com/akeronecr:1.0.0"
+    cpu   = 0
+    portMappings = [{
+      name          = "api-5000-tcp"
+      containerPort = 4000
+      hostPort      = 4000
+      protocol      = "tcp"
+      appProtocol   = "http"
+    }],
+    essential = true
     environment = [
       {
         name  = "PORT"
@@ -387,38 +150,25 @@ resource "aws_ecs_task_definition" "service" {
         value = "postgresql://akeron:Pcvinpostgres@postgresakeron.cbjrqddvgoeg.eu-central-1.rds.amazonaws.com:5432/akerondatabase"
       }
     ]
-    "mountPoints" : [],
-    "volumesFrom" : [],
-    "logConfiguration" : {
-      "logDriver" : "awslogs",
-      "options" : {
-        "awslogs-group" : "/ecs/akeron-task",
-        "mode" : "non-blocking",
-        "awslogs-create-group" : "true",
-        "max-buffer-size" : "25m",
-        "awslogs-region" : "eu-central-1",
-        "awslogs-stream-prefix" : "ecs"
-      }
-    },
-    "systemControls" : []
   }])
-  execution_role_arn = "arn:aws:iam::863872515231:role/ecsTaskExecutionRole"
-  requires_compatibilities = [
-    "EC2"
-  ]
-  cpu          = "1024"
-  memory       = "205"
-  network_mode = "awsvpc"
+  execution_role_arn       = "arn:aws:iam::863872515231:role/ecsTaskExecutionRole"
+  requires_compatibilities = ["EC2"]
+  network_mode             = "awsvpc"
+  cpu                      = "1024"
+  memory                   = "2048"
 }
+
 resource "aws_ecs_service" "ecs_service" {
-  name            = "my-ecs-service"
-  cluster         = aws_ecs_cluster.ecs.id
-  task_definition = aws_ecs_task_definition.service.arn
+  name            = "akeron-ecs-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.ecs_task_definition.arn
   desired_count   = 1
+
   network_configuration {
     subnets         = ["subnet-048ed615c746deb83", "subnet-0c446972c4be90650", "subnet-0a886674953f327bc"]
     security_groups = ["sg-0943ad0b8875a7e25"]
   }
+
   force_new_deployment = true
   placement_constraints {
     type = "distinctInstance"
@@ -428,10 +178,12 @@ resource "aws_ecs_service" "ecs_service" {
     capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
     weight            = 100
   }
+
   load_balancer {
     target_group_arn = aws_lb_target_group.ecs_tg.arn
-    container_name   = "akeron-task"
+    container_name   = "akeron-ecs-task"
     container_port   = 4000
   }
-  depends_on = [aws_autoscaling_group.asg]
+
+  depends_on = [aws_autoscaling_group.ecs_asg]
 }
